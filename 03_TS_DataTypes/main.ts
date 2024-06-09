b1bef5550b4ab7f63510c786d69d14be6ccf6c45
rimanshupatel <rimanshupatel1@gmail.com>
2024-06-09 22:40:27 +0530

    add 03_TS_DataTypes

---
+++ 03_TS_DataTypes/main.ts
@@ -0,0 +1,25 @@
+// string
+const userName:string = "rimanshu";
+
+// number
+const digits:number = 2;
+
+// boolean
+let check:boolean;
+
+check = true;
+
+// user information using ts datatypes declearation;
+
+let user:string;
+let surName:string|number;
+let age:number;
+let isLoading:boolean;
+
+user = "john"
+surName = "patel" // either it string or number like :- john1, john2
+age = 19;
+isLoading = true;
+
+// console user Data with Message;
+console.log(`hii, my name is ${user} ${surName} and my age is ${age}`)
