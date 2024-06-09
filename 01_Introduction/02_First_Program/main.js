818962c87c9840700d96fa170c0fd20f6578891b
rimanshupatel <rimanshupatel1@gmail.com>
2024-06-09 15:34:20 +0530

    02_First_Program

---
+++ 01_Introduction/02_First_Program/main.js
@@ -0,0 +1,7 @@
+// let a:number = 2;
+// let b:number = 2;
+// console.log(a*b)
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(1, 2));
---
+++ 01_Introduction/02_First_Program/main.ts
@@ -0,0 +1,20 @@
+// print name using string datatypes.
+let userName:string = "rimanshu";
+console.log(userName);
+
+// subtract two numbers using decleared variable.
+const a:number = 5;
+const b:number = 2;
+console.log(a-b);
+
+// program to add two numbers.
+function add(a:number, b:number):number{
+    return a+b;
+}
+console.log(add(3, 2));
+
+// program to multiply two numbers.
+function multiply(a:number, b:number):number{
+    return a*b;
+}
+console.log(multiply(2, 2));
