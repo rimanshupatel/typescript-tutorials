cd6e4882bf2a4b6615d969c890d8dbc26eb926af
rimanshupatel <rimanshupatel1@gmail.com>
2024-06-11 17:15:33 +0530

    add 06_TS_Function

---
+++ 06_TS_Function/main.ts
@@ -0,0 +1,16 @@
+// get time :
+function getTime(): number {
+  return new Date().getTime();
+}
+console.log(getTime());
+// program to add two numbers.
+function addNum(a: number, b: number): number {
+  return a + b;
+}
+console.log(addNum(3, 2));
+
+// program to multiply two numbers.
+function multiplyNum(a: number, b: number): number {
+  return a * b;
+}
+console.log(multiplyNum(2, 2));
