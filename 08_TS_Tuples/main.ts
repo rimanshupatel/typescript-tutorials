448e3d6c0a656ac934ae93575e80562fb45771fe
rimanshupatel <rimanshupatel1@gmail.com>
2024-06-12 10:27:52 +0530

    Add 08_TS_Tuples

---
+++ 08_TS_Tuples/main.ts
@@ -0,0 +1,8 @@
+// A tuple is a typed array with a pre-defined length and types for each index.
+// Tuples are great because they allow each element in the array to be a known type of value.
+
+let tuples: [string, string, boolean, number, number];
+tuples = ["one", "two", true, 2, 3];
+
+let users: [string, string, number, boolean];
+users = ["john", "yash", 22453, true];
